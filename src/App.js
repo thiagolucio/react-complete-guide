fc3ce647bdb062194a0b3003a9da6a493eb25144
thiagolucio <thiagolucio@gmail.com>
2020-02-23 12:23:09 -0300

    starting creating app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Aplication Course</h1>
+          <Person/>
       </div>
     );
+   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Agora estou funcionando'));
   }
+
 }
 
 export default App;
+  
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p><b>Hi,</b> I'm component Person Exported on this App</p>
+};
+
+export default person;
